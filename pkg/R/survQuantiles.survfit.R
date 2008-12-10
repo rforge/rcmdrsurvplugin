40f9bac1100827ffc17ef976dc36365911858c6e
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-10 20:50:00 +0000

    fixed survival quantiles. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/survQuantiles.survfit.R
+++ pkg/R/survQuantiles.survfit.R
@@ -1,11 +1,17 @@
-# last modified 6 December 2008 by J. Fox
+# last modified 9 December 2008 by J. Fox
 
 survQuantiles.survfit <-
-function(object, quantiles=c(.25, .5, .75), ...){
+	function(object, quantiles=c(.25, .5, .75), interpolate=FALSE, ...){
 	quants <- function(surv, t){
-		invS <- splinefun(surv, t, method="monoH.FC")
-		q <- invS(quantiles)
-		q[quantiles < min(surv) | quantiles > max(surv)] <- NA
+		if (interpolate){
+			invS <- splinefun(surv, t, method="monoH.FC")
+			q <- invS(quantiles)
+			q[quantiles < min(surv) | quantiles > max(surv)] <- NA
+		} else {
+			select <- sapply(quantiles, function(q) q >= surv)
+			posn <- apply(select, 2, function(x) min(which(x)))
+			q <- t[posn]
+		}
 		names(q) <- as.character(round(quantiles, 3))
 		q
 	}
