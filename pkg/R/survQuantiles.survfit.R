088fdf3a3dccdefd7cb6dedcfff6dd69fe954bd9
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-11 00:35:00 +0000

    conf limits for survival quantiles. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/survQuantiles.survfit.R
+++ pkg/R/survQuantiles.survfit.R
@@ -1,30 +1,38 @@
 # last modified 9 December 2008 by J. Fox
 
 survQuantiles.survfit <-
-	function(object, quantiles=c(.25, .5, .75), interpolate=FALSE, ...){
-	quants <- function(surv, t){
-		if (interpolate){
-			invS <- splinefun(surv, t, method="monoH.FC")
-			q <- invS(quantiles)
-			q[quantiles < min(surv) | quantiles > max(surv)] <- NA
-		} else {
-			select <- sapply(quantiles, function(q) q >= surv)
-			posn <- apply(select, 2, function(x) min(which(x)))
-			q <- t[posn]
-		}
-		names(q) <- as.character(round(quantiles, 3))
-		q
+	function(object, quantiles=c(.25, .5, .75), ...){
+	quants <- function(surv, lower, upper, t){
+		warn <- options(warn=-1)
+		on.exit(options(warn))
+		select <- sapply(quantiles, function(q) q >= surv)
+		posn <- apply(select, 2, function(x) min(which(x)))
+		q <- t[posn]
+		select <- sapply(quantiles, function(q) q >= lower)
+		posn <- apply(select, 2, function(x) min(which(x)))
+		low <- t[posn]
+		select <- sapply(quantiles, function(q) q >= upper)
+		posn <- apply(select, 2, function(x) min(which(x)))
+		up <- t[posn]
+		rbind(low, q, up)
 	}
 	summary <- summary(object)
+	conf.level <- object$conf.int
 	strata <- summary$strata
-	if (is.null(strata)) return(quants(summary$surv, summary$time))
-	levels <- levels(strata)
-	table <- matrix(0, length(levels), length(quantiles))
-	rownames(table) <- levels
-	colnames(table) <- as.character(round(quantiles, 3))
-	for (s in levels){
-		select <- strata == s
-		table[s, ] <- quants(summary$surv[select], summary$time[select])
+	if (is.null(strata)) {
+		table <- quants(summary$surv, summary$lower, summary$upper, summary$time)
+		dimnames(table) <- list(Estimate=c(paste("lower", conf.level, "CL"), "quantile", paste("upper", conf.level, "CL")), 
+			"Survival Probability"=as.character(round(quantiles, 3)))
+	} else {
+		levels <- levels(strata)
+		table <- array(0, c(3, length(quantiles), length(levels)))
+		dimnames(table) <- list(Estimate=c(paste("lower", conf.level, "CL"), "quantile", paste("upper", conf.level, "CL")), 
+			"Survival Probability"=as.character(round(quantiles, 3)), Stratum=levels)
+		for (s in levels){
+			select <- strata == s
+			table[, , s] <- quants(summary$surv[select], summary$lower[select], 
+				summary$upper[select], summary$time[select])
+		}
 	}
 	table
 }
