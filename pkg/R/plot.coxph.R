665c9960fc45ae41424938e4e12ef3305b94a098
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-17 00:36:00 +0000

    new PlotCoxph dialog. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/plot.coxph.R
+++ pkg/R/plot.coxph.R
@@ -1,12 +1,21 @@
-# last modified 14 December 2009 by J. Fox
+# last modified 16 December 2009 by J. Fox
 
 plot.coxph <- function(x, newdata, typical=mean, byfactors=FALSE, col=palette(), lty, ...){
 	vars <- all.vars(formula(x)[-1])
-	X <- na.omit(expand.model.frame(x, vars)[,vars]) 
-	if (byfactors){
+	XX <- X <- na.omit(expand.model.frame(x, vars)[,vars]) 
+	terms <- attr(terms(x), "term.labels")
+	tstrata <- grep("strata\\(", terms)
+	if (byfactors || !missing(newdata)){
 		if (missing(newdata)){
 			newdata <- list()
 			names <- names(X)
+			if (length(tstrata) > 0){
+				strata <- gsub("strata\\(|\\)", "", terms[tstrata])
+				strata <- strsplit(strata, "[\\ \\,]+")[[1]]
+			}
+			else strata <- NULL
+			names <- setdiff(names, strata)
+			X <- X[,names]
 			for (i in 1:ncol(X)){
 				newdata[[names[i]]] <- if (inherits(X[[i]], "factor")) levels(X[[i]])
 					else typical(X[[i]])
@@ -15,37 +24,158 @@ plot.coxph <- function(x, newdata, typical=mean, byfactors=FALSE, col=palette(),
 		}
 		S <- survfit(x, newdata=newdata)
 		newdata <-  as.data.frame(lapply(newdata, function(x) if(is.factor(x)) x else signif(x, 4)))
-		if (missing(lty)) lty <- 1:nrow(newdata)
-		col1 <- col
-		lty1 <- lty
-		if (length(grep("strata\\(", as.character(formula(x))[[3]])) > 0){
-			col <- rep(col, each=2)
-			lty <- rep(lty, each=2)
-		}
+		if (missing(lty)) lty <- 1:((1 + (length(tstrata) > 0))*nrow(newdata))
 		plot(S, col=col, lty=lty, 
 			main=paste("Survival by", paste(names(newdata), collapse=", ")), 
 			xlab="time", ylab="survival", ...)
+		if (length(tstrata)> 0){
+			levels <- levels(with(XX, eval(parse(text=terms[tstrata]))))
+			each <- nrow(newdata)
+			new <- newdata
+			for (i in 1:(length(levels) - 1)) newdata <- rbind(newdata, new)
+			newdata <- cbind(newdata, Strata=rep(levels, each=each))
+		}
 		legend("bottomleft", legend=apply(newdata, 1, function(x)  paste(x, collapse=", ")),
-			col=col1, lty=lty1, title=paste("          ", paste(names(newdata), collapse=", ")), bty="n")
+			col=col, lty=lty, title=paste("          ", paste(names(newdata), collapse=", ")), bty="n")
 	}
 	else {
 		terms <- attr(terms(x), "term.labels")
 		S <- survfit(x)
-		strata <- grep("strata\\(", terms)
-		if (length(strata) > 0) {
-			levels <- levels(with(X, eval(parse(text=terms[strata]))))
+		tstrata <- grep("strata\\(", terms)
+		if (length(tstrata) > 0) {
+			levels <- levels(with(XX, eval(parse(text=terms[tstrata]))))
 			if (missing(lty)) lty <- 1:length(levels)
-			plot(S, col=col, lty=lty, main="Survival by Strata at Average Covariates", xlab="time", ylab="survival", ...)
-			legend("bottomleft", legend=levels, col=col, lty=lty, title=terms[strata], bty="n")
+			plot(S, col=col, lty=lty, main="Survival by Strata at Average Predictors", xlab="time", ylab="survival", ...)
+			legend("bottomleft", legend=levels, col=col, lty=lty, title=terms[tstrata], bty="n")
 		}
-		else plot(S, main="Survival at Average Covariates", xlab="time", ylab="survival", ...)
+		else plot(S, main="Survival at Average Predictors", xlab="time", ylab="survival", ...)
 	}
 	invisible(summary(S))
 }
 
 PlotCoxph <- function(){
-	doItAndPrint(paste("plot(", ActiveModel(), ")", sep=""))
-}
+	setWidth <- function(x){
+		if (!is.factor(x)) 5
+		else (2 + max(nchar(as.character(x))))
+	}
+	.activeModel <- ActiveModel()
+	col.names <- all.vars(formula(get(.activeModel))[-1])
+	terms <- attr(terms(get(.activeModel)), "term.labels")
+	strata <- grep("strata\\(", terms)
+	if (length(strata) > 0){
+		strata <- gsub("strata\\(|\\)", "", terms[strata])
+		strata <- strsplit(strata, "[\\ \\,]+")[[1]]
+	}
+	col.names <- setdiff(col.names, strata)
+	X <- na.omit(expand.model.frame(get(.activeModel), col.names)[,col.names]) 
+	widths <- sapply(X, setWidth)
+	env <- environment()
+	initializeDialog(title=gettextRcmdr("Plot Cox-Model Survival Functions"))
+	radioButtons(top, name="type",
+		buttons=c("standard", "factors", "enter"), initialValue="standard",
+		labels=gettextRcmdr(c("Plot at predictor means", 
+				"Plot by factor levels at covariate means", "Plot by specified values of predictors")), 
+		title=gettextRcmdr("Type of Plot"))
+	outerTableFrame <- tkframe(top)
+	assign(".tableFrame", tkframe(outerTableFrame), envir=env)
+	setUpTable <- function(...){
+		tkdestroy(get(".tableFrame", envir=env))
+		assign(".tableFrame", tkframe(outerTableFrame), envir=env)
+		nrows <- as.numeric(tclvalue(rowsValue))
+		make.col.names <- "labelRcmdr(.tableFrame, text='')"
+		for (j in 1:ncols) {
+			make.col.names <- paste(make.col.names, ", ", 
+				"labelRcmdr(.tableFrame, text='", col.names[j], "')", sep="")
+		}
+		eval(parse(text=paste("tkgrid(", make.col.names, ")", sep="")), envir=env)
+		for (i in 1:nrows){   
+			varname <- paste(".tab.", i, ".1", sep="") 
+			assign(varname, tclVar("") , envir=env)
+			make.row <- paste("labelRcmdr(.tableFrame, text=", i, ")")
+			make.row <- paste(make.row, ", ", "ttkentry(.tableFrame, width=", 
+				widths[1], ", textvariable=", 
+				varname, ")", sep="")
+			for (j in 2:ncols){
+				varname <- paste(".tab.", i, ".", j, sep="")
+				assign(varname, tclVar(""), envir=env)
+				make.row <- paste(make.row, ", ", "ttkentry(.tableFrame, width=", 
+					widths[j], ",textvariable=", 
+					varname, ")", sep="")
+			}
+			eval(parse(text=paste("tkgrid(", make.row, ")", sep="")), envir=env)
+		}
+		tkgrid(get(".tableFrame", envir=env), sticky="w")
+	}
+	ncols <- length(col.names)
+	rowsFrame <- tkframe(top)
+	rowsValue <- tclVar("1")
+	rowsSlider <- tkscale(rowsFrame, from=1, to=10, showvalue=FALSE, variable=rowsValue,
+		resolution=1, orient="horizontal", command=setUpTable)
+	rowsShow <- labelRcmdr(rowsFrame, textvariable=rowsValue, width=2, justify="right")
+	onOK <- function(){
+		type <- as.character(tclvalue(typeVariable))
+		closeDialog()
+		if (type == "standard"){
+			command <- paste("plot(", .activeModel, ")", sep="")
+			doItAndPrint(command)
+		}
+		else if (type == "factors"){
+			command <- paste("plot(", .activeModel, ", byfactors=TRUE)", sep="")
+			doItAndPrint(command)
+		}
+		else {
+			nrows <- as.numeric(tclvalue(rowsValue))
+			cell <- 0
+			values <- rep("", nrows*ncols)
+			for (i in 1:nrows){
+				for (j in 1:ncols){
+					cell <- cell+1
+					varname <- paste(".tab.", i, ".", j, sep="")
+					values[cell] <- eval(parse(text=paste("tclvalue(", varname,")", sep="")))
+				}
+			}
+			values <- trim.blanks(values)
+			values <- values[values != ""]
+			if (length(values) != nrows*ncols){
+				Message(message=sprintf(gettextRcmdr(
+							"Number of valid entries in prediction data (%d)\nnot equal to number of rows (%d) * number of columns (%d)."), 
+						length(values), nrows, ncols), type="error")
+				PlotCoxph()
+				return()
+			}
+			values <- matrix(values, nrows, ncols, byrow=TRUE)
+			colnames(values) <- col.names
+			command <- character(0)
+			for (i in 1:ncols){
+				vals <- values[,i]
+				if (inherits(X[[i]], "factor")){
+					vals <- paste(paste('"', vals, '"', sep=""), collapse=",")
+					command <- c(command, paste(col.names[i], "=factor(c(", vals, "), levels=c(", 
+							paste(paste('"', levels(X[[i]]), '"', sep=""), collapse=","), "))",
+							sep=""))
+				}
+				else command <- c(command, paste(col.names[i], "=c(", paste(vals, collapse=","), ")", sep=""))
+			}
+			command <- paste(".newdata <- data.frame(", paste(command, collapse=","), ")", sep="")
+			doItAndPrint(command)
+			command <- paste("plot(", .activeModel, ", newdata=.newdata)", sep="")
+			doItAndPrint(command)
+			remove(.newdata, envir=.GlobalEnv)
+			logger("remove(.newdata)")
+		}
+		tkfocus(CommanderWindow())
+	}
+	OKCancelHelp(helpSubject="plot.coxph")
+	tkgrid(typeFrame, sticky="w")
+	tkgrid(labelRcmdr(top, text=""))
+	tkgrid(labelRcmdr(top, text=gettextRcmdr("Specify Values of Predictors:"), fg="blue"), sticky="w")
+	tkgrid(labelRcmdr(rowsFrame, text=gettextRcmdr("Number of rows:")), rowsSlider, rowsShow, sticky="w")
+	tkgrid(rowsFrame, sticky="w")
+	tkgrid(outerTableFrame, sticky="w")
+	tkgrid(labelRcmdr(top, text=""))
+	tkgrid(buttonsFrame, sticky="w")
+	dialogSuffix(rows=4, columns=1)       
+} 
 
 TermPlots <- function(){
 	.activeModel <- activeModel()
