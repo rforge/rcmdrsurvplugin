6b248b07b30a45ead6e180662ea2bbc02c43d44c
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-05 14:38:57 +0000

    added NAMESPACE etc. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@55 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/startup.R
+++ pkg/R/startup.R
@@ -1,11 +1,25 @@
-.First.lib <- function(libname, pkgname){
-    if (!interactive()) return()
-    Rcmdr <- options()$Rcmdr
-    plugins <- Rcmdr$plugins
-    if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-        Rcmdr$plugins <- c(plugins, pkgname)
-        options(Rcmdr=Rcmdr)
-        closeCommander(ask=FALSE, ask.save=TRUE)
-        Commander()
-        }
-    }
+# last modified 2011-07-05
+
+#.First.lib <- function(libname, pkgname){
+#    if (!interactive()) return()
+#    Rcmdr <- options()$Rcmdr
+#    plugins <- Rcmdr$plugins
+#    if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
+#        Rcmdr$plugins <- c(plugins, pkgname)
+#        options(Rcmdr=Rcmdr)
+#        closeCommander(ask=FALSE, ask.save=TRUE)
+#        Commander()
+#        }
+#    }
+
+.onAttach <- function(libname, pkgname){
+	if (!interactive()) return()
+	Rcmdr <- options()$Rcmdr
+	plugins <- Rcmdr$plugins
+	if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
+		Rcmdr$plugins <- c(plugins, pkgname)
+		options(Rcmdr=Rcmdr)
+		closeCommander(ask=FALSE, ask.save=TRUE)
+		Commander()
+	}
+}
