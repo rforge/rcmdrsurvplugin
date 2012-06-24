fd62716bc9dfe97847233e2e3adf8fe0cac140b3
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-06-24 14:05:16 +0000

    added call to globalVariables(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/globals.R
@@ -0,0 +1,10 @@
+if (getRversion() >= '2.15.1') globalVariables(c('.dfbeta', '.mfrow', '.dfbetas',
+					'top', 'tiesVariable', 'robustVariable', 'subsetVariable', 'rhsVariable',
+					'tiesFrame', 'robustFrame', 'xBox', 'outerOperatorsFrame', 'operatorsFrame',
+					'formulaFrame', 'subsetFrame', 'buttonsFrame', 'rhsEntry', '.CoxZPH', '.b',
+					'.residuals', '.X', '.fitted', '.tab.1.1', 'confintVariable', '.newdata',
+					'confintFrame', 'typeFrame', 'typeVariable', 'errorVariable', 'survtypeVariable',
+					'detailVariable', 'conftypeVariable', 'plotconfVariable', '.Survfit',
+					'survtypeFrame', 'detailFrame', 'conftypeFrame', 'plotconfFrame', 'errorFrame',
+					'factorsButton', 'allButton', 'clusterButtonsFrame', 'newVar',
+					'distributionVariable', 'distributionFrame'))
