8a4adab4c8aa7bc08fcd969eaeff28a182c0e3df
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-15 16:47:23 +0000

    eliminate double-startup. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/startup.R
+++ pkg/R/startup.R
@@ -1,25 +1,34 @@
-# last modified 2011-07-05
+# last modified 2012-12-14
 
-#.First.lib <- function(libname, pkgname){
-#    if (!interactive()) return()
-#    Rcmdr <- options()$Rcmdr
-#    plugins <- Rcmdr$plugins
-#    if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-#        Rcmdr$plugins <- c(plugins, pkgname)
-#        options(Rcmdr=Rcmdr)
-#        closeCommander(ask=FALSE, ask.save=TRUE)
-#        Commander()
-#        }
-#    }
+# .onAttach <- function(libname, pkgname){
+# 	if (!interactive()) return()
+# 	Rcmdr <- options()$Rcmdr
+# 	plugins <- Rcmdr$plugins
+# 	if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
+# 		Rcmdr$plugins <- c(plugins, pkgname)
+# 		options(Rcmdr=Rcmdr)
+# 		closeCommander(ask=FALSE, ask.save=TRUE)
+# 		Commander()
+# 	}
+# }
 
 .onAttach <- function(libname, pkgname){
-	if (!interactive()) return()
-	Rcmdr <- options()$Rcmdr
-	plugins <- Rcmdr$plugins
-	if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-		Rcmdr$plugins <- c(plugins, pkgname)
-		options(Rcmdr=Rcmdr)
-		closeCommander(ask=FALSE, ask.save=TRUE)
-		Commander()
-	}
+    if (!interactive()) return()
+    Rcmdr <- options()$Rcmdr
+    plugins <- Rcmdr$plugins
+    if (!pkgname %in% plugins) {
+        Rcmdr$plugins <- c(plugins, pkgname)
+        options(Rcmdr=Rcmdr)
+        if("package:Rcmdr" %in% search()) {
+            if(!getRcmdr("autoRestart")) {
+                Rcmdr$ask.on.exit <- FALSE
+                options(Rcmdr=Rcmdr)
+                closeCommander(ask=FALSE, ask.save=TRUE)
+                Commander()
+            }
+        }
+        else {
+            Commander()
+        }
+    }
 }
