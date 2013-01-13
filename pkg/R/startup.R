75153c8ec21f2e481a94cdb6bfed320c84903090
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-13 19:02:50 +0000

    clean up DESCRIPTION, NAMESPACE, startup.R. John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rcmdrsurvplugin@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/startup.R
+++ pkg/R/startup.R
@@ -1,16 +1,4 @@
-# last modified 2012-12-14
-
-# .onAttach <- function(libname, pkgname){
-# 	if (!interactive()) return()
-# 	Rcmdr <- options()$Rcmdr
-# 	plugins <- Rcmdr$plugins
-# 	if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-# 		Rcmdr$plugins <- c(plugins, pkgname)
-# 		options(Rcmdr=Rcmdr)
-# 		closeCommander(ask=FALSE, ask.save=TRUE)
-# 		Commander()
-# 	}
-# }
+# last modified 2013-01-13
 
 .onAttach <- function(libname, pkgname){
     if (!interactive()) return()
